f1205829e801de5478897987150c6d62e00c5603
Illya Klymov <xanf@xanf.me>
2014-08-14 12:25:07 +0300

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,13 @@
+exports.config = {
+  mail: {
+    mandrillPass: 'pass',
+    mandrill_key: 'key',
+    from: 'example@example.com',
+  },
+  twilio: {
+    "accountSid":"SID",
+    "authToken":"token",
+    "phoneNumber":"+16123516315"
+  },
+  sms: 'Your new NDG password is '
+}
